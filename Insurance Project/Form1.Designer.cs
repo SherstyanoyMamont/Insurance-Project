04ce6a6a7afd5bd2f1129feb9765c190fe0a5d9e
Ivan Korotkov <108954845+SherstyanoyMamont@users.noreply.github.com>
2025-03-13 14:25:38 +0000

    - Added calculations of date of birth - Added creation of class objects

--- Insurance Project/Form1.Designer.cs
+++ Insurance Project/Form1.Designer.cs
@@ -42,6 +42,20 @@
             comboBoxCarModel = new ComboBox();
             labelDiscount = new Label();
             label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBoxColour = new TextBox();
+            numericUpDownCarYear = new NumericUpDown();
+            label5 = new Label();
+            numericUpDownEngineSize = new NumericUpDown();
+            label6 = new Label();
+            label7 = new Label();
+            numericUpDownPenaltyPoints = new NumericUpDown();
+            numericUpDownDriverLicense = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownCarYear).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownEngineSize).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownPenaltyPoints).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownDriverLicense).BeginInit();
             SuspendLayout();
             // 
             // buttonAddClient
@@ -57,7 +71,7 @@
             // labelFirstName
             // 
             labelFirstName.AutoSize = true;
-            labelFirstName.Location = new Point(111, 160);
+            labelFirstName.Location = new Point(51, 162);
             labelFirstName.Name = "labelFirstName";
             labelFirstName.Size = new Size(97, 25);
             labelFirstName.TabIndex = 1;
@@ -67,7 +81,7 @@
             // labelSurname
             // 
             labelSurname.AutoSize = true;
-            labelSurname.Location = new Point(111, 216);
+            labelSurname.Location = new Point(51, 218);
             labelSurname.Name = "labelSurname";
             labelSurname.Size = new Size(82, 25);
             labelSurname.TabIndex = 2;
@@ -76,7 +90,7 @@
             // labelDoB
             // 
             labelDoB.AutoSize = true;
-            labelDoB.Location = new Point(111, 282);
+            labelDoB.Location = new Point(51, 274);
             labelDoB.Name = "labelDoB";
             labelDoB.Size = new Size(112, 25);
             labelDoB.TabIndex = 3;
@@ -85,7 +99,7 @@
             // checkBoxDiscount
             // 
             checkBoxDiscount.AutoSize = true;
-            checkBoxDiscount.Location = new Point(293, 443);
+            checkBoxDiscount.Location = new Point(247, 628);
             checkBoxDiscount.Name = "checkBoxDiscount";
             checkBoxDiscount.Size = new Size(78, 29);
             checkBoxDiscount.TabIndex = 4;
@@ -94,22 +108,22 @@
             // 
             // textBoxFirstName
             // 
-            textBoxFirstName.Location = new Point(293, 157);
+            textBoxFirstName.Location = new Point(188, 156);
             textBoxFirstName.Name = "textBoxFirstName";
-            textBoxFirstName.Size = new Size(300, 31);
+            textBoxFirstName.Size = new Size(321, 31);
             textBoxFirstName.TabIndex = 5;
             // 
             // textBoxSurname
             // 
-            textBoxSurname.Location = new Point(293, 216);
+            textBoxSurname.Location = new Point(188, 215);
             textBoxSurname.Name = "textBoxSurname";
-            textBoxSurname.Size = new Size(300, 31);
+            textBoxSurname.Size = new Size(321, 31);
             textBoxSurname.TabIndex = 6;
             // 
             // comboBoxCarBrand
             // 
             comboBoxCarBrand.FormattingEnabled = true;
-            comboBoxCarBrand.Location = new Point(293, 329);
+            comboBoxCarBrand.Location = new Point(687, 151);
             comboBoxCarBrand.Name = "comboBoxCarBrand";
             comboBoxCarBrand.Size = new Size(300, 33);
             comboBoxCarBrand.TabIndex = 7;
@@ -117,7 +131,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(111, 332);
+            label1.Location = new Point(557, 154);
             label1.Name = "label1";
             label1.Size = new Size(89, 25);
             label1.TabIndex = 8;
@@ -125,15 +139,15 @@
             // 
             // dateTimePickerDoB
             // 
-            dateTimePickerDoB.Location = new Point(293, 277);
+            dateTimePickerDoB.Location = new Point(188, 266);
             dateTimePickerDoB.Name = "dateTimePickerDoB";
-            dateTimePickerDoB.Size = new Size(300, 31);
+            dateTimePickerDoB.Size = new Size(321, 31);
             dateTimePickerDoB.TabIndex = 10;
             // 
             // labelCarModel
             // 
             labelCarModel.AutoSize = true;
-            labelCarModel.Location = new Point(111, 390);
+            labelCarModel.Location = new Point(557, 212);
             labelCarModel.Name = "labelCarModel";
             labelCarModel.Size = new Size(94, 25);
             labelCarModel.TabIndex = 11;
@@ -142,7 +156,7 @@
             // comboBoxCarModel
             // 
             comboBoxCarModel.FormattingEnabled = true;
-            comboBoxCarModel.Location = new Point(293, 387);
+            comboBoxCarModel.Location = new Point(687, 209);
             comboBoxCarModel.Name = "comboBoxCarModel";
             comboBoxCarModel.Size = new Size(300, 33);
             comboBoxCarModel.TabIndex = 12;
@@ -150,7 +164,7 @@
             // labelDiscount
             // 
             labelDiscount.AutoSize = true;
-            labelDiscount.Location = new Point(111, 443);
+            labelDiscount.Location = new Point(65, 628);
             labelDiscount.Name = "labelDiscount";
             labelDiscount.Size = new Size(82, 25);
             labelDiscount.TabIndex = 13;
@@ -166,11 +180,107 @@
             label2.TabIndex = 14;
             label2.Text = "Insurance Company";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(557, 266);
+            label3.Name = "label3";
+            label3.Size = new Size(65, 25);
+            label3.TabIndex = 15;
+            label3.Text = "Colour";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(557, 315);
+            label4.Name = "label4";
+            label4.Size = new Size(44, 25);
+            label4.TabIndex = 16;
+            label4.Text = "Year";
+            // 
+            // textBoxColour
+            // 
+            textBoxColour.Location = new Point(687, 266);
+            textBoxColour.Name = "textBoxColour";
+            textBoxColour.Size = new Size(300, 31);
+            textBoxColour.TabIndex = 17;
+            // 
+            // numericUpDownCarYear
+            // 
+            numericUpDownCarYear.Location = new Point(687, 316);
+            numericUpDownCarYear.Maximum = new decimal(new int[] { 2025, 0, 0, 0 });
+            numericUpDownCarYear.Minimum = new decimal(new int[] { 1990, 0, 0, 0 });
+            numericUpDownCarYear.Name = "numericUpDownCarYear";
+            numericUpDownCarYear.Size = new Size(300, 31);
+            numericUpDownCarYear.TabIndex = 2020;
+            numericUpDownCarYear.Value = new decimal(new int[] { 2020, 0, 0, 0 });
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(557, 365);
+            label5.Name = "label5";
+            label5.Size = new Size(101, 25);
+            label5.TabIndex = 2021;
+            label5.Text = "Engine Size";
+            // 
+            // numericUpDownEngineSize
+            // 
+            numericUpDownEngineSize.Increment = new decimal(new int[] { 1, 0, 0, 131072 });
+            numericUpDownEngineSize.Location = new Point(687, 365);
+            numericUpDownEngineSize.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            numericUpDownEngineSize.Name = "numericUpDownEngineSize";
+            numericUpDownEngineSize.Size = new Size(300, 31);
+            numericUpDownEngineSize.TabIndex = 2022;
+            numericUpDownEngineSize.Value = new decimal(new int[] { 15, 0, 0, 65536 });
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(51, 322);
+            label6.Name = "label6";
+            label6.Size = new Size(120, 25);
+            label6.TabIndex = 2023;
+            label6.Text = "Driver License";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(51, 371);
+            label7.Name = "label7";
+            label7.Size = new Size(121, 25);
+            label7.TabIndex = 2024;
+            label7.Text = "Penalty Points";
+            // 
+            // numericUpDownPenaltyPoints
+            // 
+            numericUpDownPenaltyPoints.Location = new Point(188, 371);
+            numericUpDownPenaltyPoints.Name = "numericUpDownPenaltyPoints";
+            numericUpDownPenaltyPoints.Size = new Size(321, 31);
+            numericUpDownPenaltyPoints.TabIndex = 2025;
+            // 
+            // numericUpDownDriverLicense
+            // 
+            numericUpDownDriverLicense.Location = new Point(188, 320);
+            numericUpDownDriverLicense.Name = "numericUpDownDriverLicense";
+            numericUpDownDriverLicense.Size = new Size(321, 31);
+            numericUpDownDriverLicense.TabIndex = 2026;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1034, 710);
+            Controls.Add(numericUpDownDriverLicense);
+            Controls.Add(numericUpDownPenaltyPoints);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(numericUpDownEngineSize);
+            Controls.Add(label5);
+            Controls.Add(numericUpDownCarYear);
+            Controls.Add(textBoxColour);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(labelDiscount);
             Controls.Add(comboBoxCarModel);
@@ -187,6 +297,10 @@
             Controls.Add(buttonAddClient);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDownCarYear).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownEngineSize).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownPenaltyPoints).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownDriverLicense).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -207,5 +321,15 @@
         private ComboBox comboBoxCarModel;
         private Label labelDiscount;
         private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBoxColour;
+        private NumericUpDown numericUpDownCarYear;
+        private Label label5;
+        private NumericUpDown numericUpDownEngineSize;
+        private Label label6;
+        private Label label7;
+        private NumericUpDown numericUpDownPenaltyPoints;
+        private NumericUpDown numericUpDownDriverLicense;
     }
 }
--- Insurance Project/Form1.cs
+++ Insurance Project/Form1.cs
@@ -1,13 +1,10 @@
+using System.Collections.Generic;
+using System.Windows.Forms;
+
 namespace Insurance_Project
 {
     public partial class Form1 : Form
     {
-        public int totalCost;
-        public int normalCost = 1000;
-        public int under24ageCoeficient = 30; // 30%
-        public int under20ageCoeficient = 30; // 30%
-
-
         public Form1()
         {
             InitializeComponent();
@@ -20,13 +17,29 @@ namespace Insurance_Project
 
         private void buttonAddClient_Click(object sender, EventArgs e)
         {
-            CalculateTotalCost();
+            DateTime birthDate = dateTimePickerDoB.Value;
+            DateTime today = DateTime.Today;
+
+            int age = today.Year - birthDate.Year;
+
+            if (birthDate.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            List<Car> Cars = new();
+
+            Car car = new Car(comboBoxCarBrand.Text, comboBoxCarModel.Text, textBoxColour.Text, (int)numericUpDownCarYear.Value, (int)numericUpDownEngineSize.Value);
+
+            Cars.Add(car);
+
+            Client client = new(textBoxFirstName.Text, textBoxSurname.Text, age, (int)numericUpDownDriverLicense.Value, (int)numericUpDownPenaltyPoints.Value, Cars);
+            
+            Insurance insurance = new(client);
+            
+            
         }
 
-        private void CalculateTotalCost()
-        {
-            totalCost = normalCost * under24ageCoeficient;
-        }
     }
 
     public abstract class BaseEntity
@@ -36,31 +49,65 @@ namespace Insurance_Project
 
     public class Insurance : BaseEntity
     {
-        public int totalCost { get; private set; }
-        public int normalCost { get; private set; }
-        public int under24ageCoeficient { get; private set; }
-        public Client client { get; private set; }
+        
+
+        public int TotalCost { get; private set; }
+        public int NormalCost { get; private set; }
+        public int Under24ageCoeficient { get; private set; }
+        public Client Client { get; private set; }
+        public Insurance(Client client)
+        {
+            this.NormalCost = 1000;
+            this.Under24ageCoeficient = 30; //30%
+            this.Client = client;
+        }
+
+        private void CalculateTotalCost()
+        {
+            TotalCost = NormalCost * Under24ageCoeficient;
+        }
 
     }
 
     public class Client : BaseEntity
     {
-        public string firstName { get; private set; }
-        public string lastName { get; private set; }
-        public int age { get; private set; }
-        public int driverLicese { get; private set; }
-        public int penaltyPoints { get; private set; }
-        public List<Car> cars { get; private set; }
+        public string? FirstName { get; private set; }
+        public string? LastName { get; private set; }
+        public int Age { get; private set; }
+        public int DriverLicese { get; private set; }
+        public int PenaltyPoints { get; private set; }
+        public List<Car> Cars { get; private set; }
 
+        public Client(string firstName, string lastName, int age, int driverLicese, int penaltyPoints, List<Car> cars)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            DriverLicese = driverLicese;
+            PenaltyPoints = penaltyPoints;
+            Cars = cars;
+
+        }
     }
 
     public class Car : BaseEntity
     {
-        public string brand { get; private set; }
-        public string model { get; private set; }
-        public int year { get; private set; }
-        public double engineSize { get; private set; }
-        public string colour { get; private set; }
-    }
+        
 
+        public string? Brand { get; private set; }
+        public string? Model { get; private set; }
+        public string? Colour { get; private set; }
+        public int Year { get; private set; }
+        public double EngineSize { get; private set; }
+
+        public Car(string brand, string model, string colour, int year, double engineSize)
+        {
+            Brand = brand;
+            Model = model;
+            Colour = colour;
+            Year = year;
+            EngineSize = engineSize;
+        }
+
+    }
 }
